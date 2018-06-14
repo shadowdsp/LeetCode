18dd12a3a29996779945efe035e8ecc698bf3a0a
shadowdsp <245121826@qq.com>
2018-06-14 21:57:39 +0800

    暴力

---
+++ 236. 二叉树的最近公共祖先/main.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    TreeNode ans;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        ans = null;
+        dfs(root, p, q);
+        return ans;
+    }
+        
+    TreeNode dfs(TreeNode rt, TreeNode p, TreeNode q) {
+        if (rt == null) return null;
+        TreeNode a = dfs(rt.left, p, q);
+        TreeNode b = dfs(rt.right, p, q);
+        TreeNode pp = null, qq = null;
+        if (a == p || b == p || rt == p) pp = p;
+        if (a == q || b == q || rt == q) qq = q;
+        if (pp != null && qq != null) {
+            if (ans == null) ans = rt;
+            return ans;
+        }
+        return pp == null ? qq : pp;
+    }
+}
