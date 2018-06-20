f007fcd90d81ecd55b57d7633ff5cbde1b87cab9
shadowdsp <245121826@qq.com>
2018-06-20 14:40:40 +0800

    后缀和

---
+++ 238. 除自身以外数组的乘积/main.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        ans[n-1] = nums[n-1];
+        for (int i = n - 2; i >= 0; i--) 
+            ans[i] = ans[i+1] * nums[i];
+        int sum = 1;
+        for (int i = 0; i < n; i++) {
+            ans[i] = ans[i+1] * sum;
+            sum *= nums[i];
+        }
+        ans[n-1] = sum;
+        return ans;
+    }
+}
