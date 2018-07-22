84e487116e6908c77d939bf42abde569a4e9da8d
shadowdsp <245121826@qq.com>
2018-07-22 14:49:53 +0800

    进制

---
+++ 470. 用 Rand7() 实现 Rand10()/main.java
@@ -0,0 +1,14 @@
+/**
+ * The rand7() API is already defined in the parent class SolBase.
+ * public int rand7();
+ * @return a random integer in the range 1 to 7
+ */
+class Solution extends SolBase {
+    public int rand10() {
+        while (true) {
+            int num = (rand7() - 1) * 7 + rand7();
+            if (1 <= num && num <= 10) 
+                return num;
+        }
+    }
+}
