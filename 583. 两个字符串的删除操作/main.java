967e5a0187cbd5680e91840b4c701dcba8ec4729
shadowdsp <shadowdsp@outlook.com>
2018-09-04 21:08:32 +0800

    dp记录

---
+++ 583. 两个字符串的删除操作/main.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int n = word1.length(), m = word2.length();
+        int[][] dp = new int[n+1][m+1];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i+1][j+1] = Math.max(dp[i][j] + 1, dp[i+1][j+1]);
+                } else {
+                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+        return n + m - 2 * dp[n][m];
+    }
+}
